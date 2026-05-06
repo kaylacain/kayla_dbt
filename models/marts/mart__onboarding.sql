b63494de0d9a12ad001a506a1f6507af383c8ab3
Kayla Cain <kaylacain95@gmail.com>
2026-05-06 02:10:34 +0000

    kc 2

---
+++ models/marts/mart__onboarding.sql
@@ -0,0 +1,17 @@
+with
+
+base as (
+
+    select * from {{ ref('int_gong_pilot__rep_performance') }}
+
+),
+
+new_hires as (
+
+    select *
+    from base
+    where is_new_hire = 'Yes'
+
+)
+
+select * from new_hires
